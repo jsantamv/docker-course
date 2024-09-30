d6d7e8471e65861f2f90930cfc381c40db29feaa
Juan Carlos Santamaria v <jsantamv@gmail.com>
2024-09-29 18:37:17 -0600

    Creacion del docker file

---
+++ cron-ticket/app.js
@@ -0,0 +1,10 @@
+var cron = require('node-cron');
+
+let times = 0;
+
+cron.schedule('1-59 * * * * * *', () => {
+    times++;
+    console.log('running a task every minute ', times);
+});
+
+console.log("inicio");
